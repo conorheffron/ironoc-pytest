2ec1eb0fb2225394623a1a6495f8beed6525df39
Conor Heffron <conor.heffron@gmail.com>
2024-10-12 09:06:58 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,13 @@
 from setuptools import setup, find_packages
+from pip.req import parse_requirements
 
+install_reqs = parse_requirements('requirements.txt', session='hack')
+
+reqs = [str(ir.req) for ir in install_reqs]
 
 setup(
     name="ironoc-pytest",
-    version="1.4.2",
+    version="1.3.3",
     packages=find_packages(),
+    install_requires=reqs,
 )
